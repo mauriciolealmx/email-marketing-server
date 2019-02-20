f444a2fcfc6527e79bcab644e0de0c26bd358b5d
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-20 11:49:05 -0600

    Installed passport and implemented googleStrategy.

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'There' });
-});
+passport.use(
+  // Google strategy gets registered as 'google'.
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      // The user will be sent to this URL after granting permission.
+      callbackURL: '/auth/google/callback',
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

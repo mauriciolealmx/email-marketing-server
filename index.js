bc890edb11761c10be7f895a0ed08cec29dae9de
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-21 15:27:04 -0600

    Make sure express serves React's index.html and static files in prod.

--- index.js
+++ index.js
@@ -27,5 +27,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve React production assets.
+  app.use(express.static('client/build'));
+
+  // Serve React's index.html tile
+  // if it does not recognize the route.
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

236d9b237e7c5820be84a84b248108f703786d0f
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-21 14:44:58 -0600

    Created default landing page component,

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 
 class App extends Component {
   componentDidMount() {
@@ -16,7 +17,7 @@ class App extends Component {
         <BrowserRouter>
           <div>
             <Header />
-            <Route exact path="/" component={() => <h1>Im the landing route</h1>} />
+            <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={() => <h1>Surveys route</h1>} />
             <Route path="/surveys/new" component={() => <h1>New Surveys route</h1>} />
           </div>

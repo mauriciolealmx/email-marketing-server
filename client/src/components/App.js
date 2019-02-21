7627c8aa9333d0aaf46b9e18ec5a0a6e52aab976
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-21 12:01:47 -0600

    Created Header component and updated App to have some dummy routes.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-  return <div>Hello there</div>;
-};
+import Header from './Header';
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={() => <h1>Im the landing route</h1>} />
+            <Route exact path="/surveys" component={() => <h1>Surveys route</h1>} />
+            <Route path="/surveys/new" component={() => <h1>New Surveys route</h1>} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+
+      default:
+        return <li><a href="">Logout</a></li>;
+    }
+  }
+
+  render() {
+    console.log(this.props.auth);
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Email Marketing</a>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);

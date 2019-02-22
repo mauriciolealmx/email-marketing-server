b0fcb1bca139431cff8603b87428c4619c7d3d27
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-22 17:48:01 -0600

    Added toggling from Review to Create.

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import ServeyForm from './SurveyFrom';
+import ServeyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
+import { reduxForm } from 'redux-form';
 
 class SurveyNew extends Component {
+  state = { showformReview: false };
+
+  renderContent() {
+    if (this.state.showformReview) {
+      return <SurveyFormReview onCancel={() => this.setState({ showformReview: false })} />;
+    }
+    return <ServeyForm onSurveyFormSubmit={() => this.setState({ showformReview: true })} />;
+  }
+
   render() {
-    return (
-      <div>
-        <ServeyForm />
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
 

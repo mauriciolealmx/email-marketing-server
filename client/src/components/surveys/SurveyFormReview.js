17fb141e00d084c43cc1789151c2d5aaaa0fb225
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-22 17:46:52 -0600

    Updated SurveyFormReview to include all fields and values from the form.

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = formFields.map(({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      {reviewFields}
+      <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
         Back
+      </button>
+      <button className="green btn-flat right white-text" onClick={() => this.props.submitSurvey(formValues)}>
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
 };
 
-export default SurveyFormReview;
+const mapStateToProps = state => {
+  return {
+    formValues: state.form.surveyForm.values,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  actions
+)(SurveyFormReview);

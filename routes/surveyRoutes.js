b57f3a7003f2cd522c8a8b71a4ae809c34c7fb4f
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-22 11:41:48 -0600

    Created surveyRoutes and updated survey template.

---
+++ routes/surveyRoutes.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const Mailer = require('../services/Mailer');
+
+const Survey = mongoose.model('surveys');
+
+module.exports = app => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    console.log('----- What do I get here', req.body);
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+      await survey.save();
+      // req.user.credits -= 1;
+      // const user = await req.user.save();
+
+      res.send(req.user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
+};
